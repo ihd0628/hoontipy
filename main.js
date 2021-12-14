4770f6fc5ccd3d8e05c3d4c991eb324b00243b51
Seunghoon Lee <94297973+ihd0628@users.noreply.github.com>
2021-12-14 15:48:33 +0900

    메뉴버튼 로그인 생성
    메뉴바 우측 로그인 아이콘 및 변환기능 생성
    -. Prompt를 통해 받은 username 저장 수단 필요
    -. 지금은 그저 껍대기만 있음.
    -. username을 받아서 기존에 user가 추가했던 본인의 음악목록을 불러올수있게 해야함.
       -> 아직 멀었지

--- main.js
+++ main.js
@@ -1,6 +1,18 @@
 const toggleBtn = document.querySelector('.Header_toggleBtn');
 const menu = document.querySelector('.Header_Menu');
 const icons = document.querySelector('.Header_icons');
+const loginIcons = document.querySelector('.Header_icons #login_icons')
+const userIcons = document.querySelector('.Header_icons #user_icons')
+
+function onloginHandle(event){
+    event.preventDefault();
+    const greeting = prompt('What`s yout name? ', 'Please write your name');
+    console.dir(greeting);
+    loginIcons.classList.add("hidden");
+    userIcons.classList.remove("hidden");
+}
+
+loginIcons.addEventListener("click",onloginHandle);
 
 toggleBtn.addEventListener('click', function(){
     menu.classList.toggle('active');
